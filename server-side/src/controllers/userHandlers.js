fdea625e8eaf2c1937a93e4cf9efa1d575978625
tpauldike <topman4loveworld@gmail.com>
2023-07-12 11:11:04 +0000

    Done with userLogin API

--- server-side/src/controllers/userHandlers.js
+++ server-side/src/controllers/userHandlers.js
@@ -7,14 +7,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const secretKey = process.env.HIDDEN_STRING;
+const promisifiedQuery = util.promisify(db.query).bind(db);
 
 //user signup
-export const createUser = async(req, res) => {
+export const createUser = async (req, res) => {
     try {
         const { firstname, lastname, othername, sex, email, phone, password, role, address } = req.body;
 
-        const promisifiedQuery = util.promisify(db.query).bind(db);     
-        const checkUserEmail = await promisifiedQuery("SELECT * FROM user WHERE email = ?", [ email ]);
+        const checkUserEmail = await promisifiedQuery("SELECT * FROM user WHERE email = ?", [email]);
 
         if (checkUserEmail.length > 0) {
             return res.status(409).json({ message: "User with this email already exists!" })
@@ -25,7 +25,7 @@ export const createUser = async(req, res) => {
 
         const newlyCreatedUser = await promisifiedQuery("INSERT INTO user (user_id, firstname, lastname, othername, sex, email, phone, password, role, address) VALUES (?,?,?,?,?,?,?,?,?,?)", [user_id, firstname, lastname, othername, sex, email, phone, hashedPassword, role, address]);
 
-        const [ , firstName, SurName, , userGender, userEmail, userPhone, , userRole, userAddress ] = newlyCreatedUser.values;
+        const [, firstName, SurName, , userGender, userEmail, userPhone, , userRole, userAddress] = newlyCreatedUser.values;
 
         const token = webToken.sign({ user_id }, secretKey, { expiresIn: "1hr" });
 
@@ -33,24 +33,32 @@ export const createUser = async(req, res) => {
 
     } catch (error) {
         console.log("Error creating new user:", error.message);
-        return res.status(500).json({ message: "Internal Server Error!"});
+        return res.status(500).json({ message: "Internal Server Error!" });
     };
 };
 
 //user login
-export const loginUser = async(req, res) => {
-try {
-    const { email, password } = req.body;
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
-    const promisifiedQuery = util.promisify(db.query).bind(db);     
-    const checkUserEmail = await promisifiedQuery("SELECT * FROM user WHERE email = ?", [ email ]);
+        const promisifiedQuery = util.promisify(db.query).bind(db);
+        const checkedUser = await promisifiedQuery("SELECT * FROM user WHERE email = ?", [email]);
 
-    if (checkUserEmail.length === 0) {
-        return res.status(404).json({ message: "Email or password incorrect!!" })
+        if (checkedUser.length === 0) {
+            return res.status(403).json({ message: "Email or password incorrect!!" });
+        }
+        const passwordValidated = await bcrypt.compare(password, checkedUser[0].password);
+        if (!passwordValidated) {
+            return res.status(403).json({ message: "Email or password incorrect!!" });
+        }
+
+        const token = webToken.sign({ business_id: checkedUser }, secretKey, { expiresIn: "1hr" });
+
+        return res.status(200).json({ message: `${checkedUser[0].firstname} successfully logged in`, token });
+
+    } catch (error) {
+        console.log("Error signing in:", error.message);
+        return res.status(500).json({ message: "Internal Server Error!" });
     }
-
-    
-} catch (error) {
-    
 }
-};
