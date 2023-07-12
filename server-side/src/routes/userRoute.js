045a10d7f31219c84c7003cc49373734f7b4d9a4
tpauldike <topman4loveworld@gmail.com>
2023-07-12 03:29:57 +0000

    login route set

--- server-side/src/routes/userRoute.js
+++ server-side/src/routes/userRoute.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createUser } from "../controllers/userHandlers.js";
+import { createUser, loginUser } from "../controllers/userHandlers.js";
 
 const router = express.Router();
 
 router.post("/sign_up", createUser);
-// router.post("/sign_in", handler);
+router.post("/sign_in", loginUser);
 
 
 export default router;
