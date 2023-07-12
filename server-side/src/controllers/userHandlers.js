31279d1c2a25bcfbfe9d69c11157a044cbb9cdd6
tpauldike <topman4loveworld@gmail.com>
2023-07-12 03:26:31 +0000

    Completed createUser API and started loginUser

--- server-side/src/controllers/userHandlers.js
+++ server-side/src/controllers/userHandlers.js
@@ -8,16 +8,22 @@ dotenv.config();
 
 const secretKey = process.env.HIDDEN_STRING;
 
+//user signup
 export const createUser = async(req, res) => {
     try {
         const { firstname, lastname, othername, sex, email, phone, password, role, address } = req.body;
 
-const checkExistingEmail = await db.query("SELECT * FROM user WHERE email = ?", [ email ]);
+        const promisifiedQuery = util.promisify(db.query).bind(db);     
+        const checkUserEmail = await promisifiedQuery("SELECT * FROM user WHERE email = ?", [ email ]);
+
+        if (checkUserEmail.length > 0) {
+            return res.status(409).json({ message: "User with this email already exists!" })
+        }
 
         const user_id = uuid.v4();
         const hashedPassword = await bcrypt.hash(password, 15);
 
-        const newlyCreatedUser = await db.query("INSERT INTO user (user_id, firstname, lastname, othername, sex, email, phone, password, role, address) VALUES (?,?,?,?,?,?,?,?,?,?)", [user_id, firstname, lastname, othername, sex, email, phone, hashedPassword, role, address]);
+        const newlyCreatedUser = await promisifiedQuery("INSERT INTO user (user_id, firstname, lastname, othername, sex, email, phone, password, role, address) VALUES (?,?,?,?,?,?,?,?,?,?)", [user_id, firstname, lastname, othername, sex, email, phone, hashedPassword, role, address]);
 
         const [ , firstName, SurName, , userGender, userEmail, userPhone, , userRole, userAddress ] = newlyCreatedUser.values;
 
@@ -29,4 +35,22 @@ const checkExistingEmail = await db.query("SELECT * FROM user WHERE email = ?", 
         console.log("Error creating new user:", error.message);
         return res.status(500).json({ message: "Internal Server Error!"});
     };
+};
+
+//user login
+export const loginUser = async(req, res) => {
+try {
+    const { email, password } = req.body;
+
+    const promisifiedQuery = util.promisify(db.query).bind(db);     
+    const checkUserEmail = await promisifiedQuery("SELECT * FROM user WHERE email = ?", [ email ]);
+
+    if (checkUserEmail.length === 0) {
+        return res.status(404).json({ message: "Email or password incorrect!!" })
+    }
+
+    
+} catch (error) {
+    
+}
 };
