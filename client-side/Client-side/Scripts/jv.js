497266d88ba84e0475e2c1d77eb21bc5c8f7abe5
tpauldike <topman4loveworld@gmail.com>
2023-07-12 06:37:15 +0000

    added the frontend codes from Moses Isang

---
+++ client-side/Client-side/Scripts/jv.js
@@ -0,0 +1,8 @@
+document.getElementById("clientSignup").addEventListener("onclick", function() {
+var form = getElementById("myForm");
+if (form.style.display === "none") {
+    form.style.display = "block";
+} else {
+    form.style.display = "none";
+}
+});
---
+++ client-side/Client-side/Scripts/kin2.js
@@ -0,0 +1,4 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById("category-dropdown");
+    dropdown.style.display = (dropdown.style.display === "none") ? "block" : "none";
+  }
